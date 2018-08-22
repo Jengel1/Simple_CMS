bdc54cef8ed123142d986f24dd2f931cd26018a4
Jengel1 <38148097+Jengel1@users.noreply.github.com>
2018-08-22 11:04:51 -0600

    added public section to site

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,13 +3,13 @@ class PagesController < ApplicationController
     layout "admin"
 
     before_action :confirm_logged_in
-    before_action :find_subjects, :only => [:new, :create, :edit, :update]
+    before_action :find_subject
     before_action :set_page_count, :only => [:new, :create, :edit, :update]
 
     #read actions
     #HTTP GET, displays list of records  GET/pages
     def index
-        @pages = Page.sorted
+        @pages = @subject.pages.sorted
     end
     #HTTP GET, displays detailed view of single record GET/pages:id
     def show
@@ -19,17 +19,18 @@ class PagesController < ApplicationController
     #create actions
     #HTTP GET, create new object to add values to GET/new_page_path
     def new
-        @page = Page.new
+        @page = Page.new(:subject_id => @subject.id)
     end
     #HTTP POST, adds new object to the database POST/pages_path
     def create
         # Instantiate new object using form params
         @page = Page.new(page_params) #use private method below to get permited data for mass assignment
+        @page.subject = @subject
         # Save object
         if @page.save
             # If successful, redirect to index action
             flash[:notice] = "Page created successfully."
-            redirect_to(pages_path)
+            redirect_to(pages_path(:subject_id => @subject.id))
         else
             # Else, redisplay form so user can fix problems
             render('new') # renders form template with original user data pre-populated
@@ -49,7 +50,7 @@ class PagesController < ApplicationController
         if @page.update_attributes(page_params) #use private method below to get permited data for mass assignment
         # If successful, redirect to show action
         flash[:notice] = "Page updated successfully."
-        redirect_to(page_path(@page))
+        redirect_to(page_path(@page, :subject_id => @subject.id))
         else
         # Else, redisplay form so user can fix problems
         render('edit') # renders form template with original pre-populated
@@ -69,20 +70,20 @@ class PagesController < ApplicationController
         @page.destroy
         # redirect back to index
         flash[:notice] = "Page '#{@page.name}' deleted successfully."
-        redirect_to(pages_path)
+        redirect_to(pages_path(:subject_id => @subject.id))
     end
 
     private
     def page_params
-        params.require(:page).permit(:subject_id, :name, :position, :visible, :permalink, :created_at)
+        params.require(:page).permit(:name, :position, :visible, :permalink, :created_at)
     end
 
-    def find_subjects
-        @subjects = Subject.sorted
+    def find_subject
+        @subject = Subject.find(params[:subject_id])
     end
 
     def set_page_count
-        @page_count = Page.count
+        @page_count = @subject.pages.count
         if params[:action] == 'new' || params[:action] == 'create'
             @page_count += 1
         end
--- app/controllers/sections_controller.rb
+++ app/controllers/sections_controller.rb
@@ -3,11 +3,13 @@ class SectionsController < ApplicationController
     layout "admin"
 
     before_action :confirm_logged_in
+    before_action :find_page
+    before_action :set_section_count, :only => [:new, :create, :edit, :update]
 
     #read actions
     #HTTP GET, displays list of records  GET/sections
     def index
-        @sections = Section.sorted
+        @sections = @page.sections.sorted
     end
     #HTTP GET, displays detailed view of single record GET/sections:id
     def show
@@ -17,23 +19,20 @@ class SectionsController < ApplicationController
     #create actions
     #HTTP GET, create new object to add values to GET/new_section_path
     def new
-      @section = Section.new
-      @section_count = Section.count + 1
-      @pages = Page.sorted
+      @section = Section.new(:page_id => @page.id)
     end
     #HTTP POST, adds new object to the database POST/sections_path
     def create
       # Instantiate new object using form params
       @section = Section.new(section_params) #use private method below to get permited data for mass assignment
+      @section.page = @page
       # Save object
       if @section.save
           # If successful, redirect to index action
           flash[:notice] = "Section created successfully."
-          redirect_to(sections_path)
+          redirect_to(sections_path(:page_id => @page.id))
       else
           # Else, redisplay form so user can fix problems
-          @section_count = Section.count + 1
-          @pages = Page.sorted
           render('new') # renders form template with original user data pre-populated
       end
     end
@@ -42,8 +41,6 @@ class SectionsController < ApplicationController
     #HTTP GET, display the form GET/edit_section_path(:id)
     def edit
       @section = Section.find(params[:id])
-      @section_count = Section.count
-      @pages = Page.sorted
     end
     #HTTP PATCH, process the form PATCH/section_path(:id)
     def update
@@ -53,11 +50,9 @@ class SectionsController < ApplicationController
       if @section.update_attributes(section_params) #use private method below to get permited data for mass assignment
           # If successful, redirect to show action
           flash[:notice] = "Section updated successfully."
-          redirect_to(section_path(@section))
+          redirect_to(section_path(@section, :page_id => @page.id))
       else
           # Else, redisplay form so user can fix problems
-          @section_count = Section.count
-          @pages = Page.sorted
           render('edit') # renders form template with original pre-populated
       end
     end
@@ -75,12 +70,24 @@ class SectionsController < ApplicationController
       @section.destroy
       # redirect back to index
       flash[:notice] = "Section '#{@section.name}' deleted successfully."
-      redirect_to(sections_path)
+      redirect_to(sections_path(:page_id => @page.id))
     end
 
     private
+
     def section_params
-      params.require(:section).permit(:page_id, :name, :position, :visible, :content_type, :content, :created_at)
+      params.require(:section).permit(:name, :position, :visible, :content_type, :content, :created_at)
+    end
+
+    def find_page
+        @page = Page.find(params[:page_id])
+    end
+
+    def set_section_count
+        @section_count = @page.sections.count
+        if params[:action] == 'new' || params[:action] == 'create'
+            @section_count += 1
+        end
     end
 
 end
